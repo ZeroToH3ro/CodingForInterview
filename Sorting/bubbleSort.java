7023a0ed2afba0696e623ad2af5f8f4ddb61a12f
ZeroToH3ro <mtblaser2002@gmail.com>
2022-07-11 13:06:27 +0700

    update bubble sort

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -4,12 +4,17 @@ public class bubbleSort {
     public static void bubbleSorting(int []nums){
         int sizeOfNums = nums.length;
         for(int i=0; i<sizeOfNums; i++){
+            boolean isChecked = true;
             for(int j=0; j<sizeOfNums-i-1; j++){
                 if(nums[j]>nums[j+1]){
+                    isChecked = false;
                     int temp = nums[j];
                     nums[j] = nums[j+1];
                     nums[j+1] = temp;
                 }
+            }
+            if(isChecked){
+                break;
             }
         }
     }
