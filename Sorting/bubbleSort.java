2afbee0dfc23b8e56cf783946469f3300c56e313
ZeroToH3ro <mtblaser2002@gmail.com>
2022-07-11 13:03:51 +0700

    bubble sort

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,26 @@
+package Progress.Sorting;
+
+public class bubbleSort {
+    public static void bubbleSorting(int []nums){
+        int sizeOfNums = nums.length;
+        for(int i=0; i<sizeOfNums; i++){
+            for(int j=0; j<sizeOfNums-i-1; j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void print(int []nums){
+        for (int i : nums) {
+            System.out.print(i+"\t");
+        }
+    }
+    public static void main(String[] args) {
+        int []nums = {2,5,6,4,2,1,0};
+        bubbleSorting(nums);
+        print(nums);
+    }
+}
