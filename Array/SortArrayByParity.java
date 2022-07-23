c6fa1a0daf0e681e6733b97726a041a91634f75f
ZeroToH3ro <mtblaser2002@gmail.com>
2022-07-23 15:27:12 +0700

    sort by parity

---
+++ Array/SortArrayByParity.java
@@ -0,0 +1,30 @@
+package Progress.Array;
+
+public class SortArrayByParity {
+    public static int[] sortArrayByParity(int[] nums) {
+        int left = 0; 
+        int right = nums.length - 1; 
+        while(left < right ) 
+        { 
+            if(nums[left] % 2 != 0) 
+            { 
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                right--;
+            } 
+            else 
+            { 
+                left++; 
+            } 
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int []nums = {3,1,2,4};
+        for (int i : sortArrayByParity(nums)) {
+            System.out.print(i+"\t");
+        };
+        System.out.println();
+    }
+}
