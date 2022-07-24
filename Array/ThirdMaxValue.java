742efc90585f27313d32cd36dcd72045400148c5
ZeroToH3ro <mtblaser2002@gmail.com>
2022-07-24 14:26:27 +0700

    third value

---
+++ Array/ThirdMaxValue.java
@@ -0,0 +1,31 @@
+package Progress.Array;
+
+public class ThirdMaxValue {
+    public static int thirdMax(int[] nums) {
+        Integer[] largest = new Integer[3];
+        for (Integer n : nums) {
+            // update first max value
+            if (largest[0] == null || n > largest[0]) {
+                largest[2] = largest[1];
+                largest[1] = largest[0];
+                largest[0] = n;
+                // update second max value
+            } else if ((largest[1] == null || n > largest[1])
+                    && (largest[0] != null && n < largest[0])) {
+                largest[2] = largest[1];
+                largest[1] = n;
+                // update third max value
+            } else if ((largest[2] == null || n > largest[2])
+                    && (largest[0] != null && n < largest[0])
+                    && (largest[1] != null && n < largest[1])) {
+                largest[2] = n;
+            }
+        }
+        return largest[2] == null ? largest[0] : largest[2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 2, 3 };
+        System.out.println(thirdMax(nums));
+    }
+}
